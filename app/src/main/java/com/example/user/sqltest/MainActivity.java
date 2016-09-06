87dff50240b7116e33e3bd53f06762a6b8412f53
user <left_handed_rocker@hotmail.co.uk>
2016-09-06 09:59:35 +0100

    0609 10:00 version

--- app/src/main/java/com/example/user/sqltest/MainActivity.java
+++ app/src/main/java/com/example/user/sqltest/MainActivity.java
@@ -5,6 +5,7 @@ package com.example.user.sqltest;
         import android.database.sqlite.SQLiteDatabase;
         import android.support.v7.app.ActionBarActivity;
         import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
         import android.view.Menu;
         import android.view.MenuItem;
         import android.view.View;
@@ -13,7 +14,7 @@ package com.example.user.sqltest;
         import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editTextName;
     private EditText editTextAdd;
